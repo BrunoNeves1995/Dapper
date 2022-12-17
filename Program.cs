9fcd0d3037353a14fb141656a206e9ac26812e09
Bruno Neves <nevesbruno814@gmail.com>
2022-12-17 10:45:18 -0400

    funcionando corretamente, abre apenas uma conexão e executa o insert e selectr

--- Program.cs
+++ Program.cs
@@ -17,77 +17,69 @@ namespace Dapper
             categoryInert.Title = "Java";
             categoryInert.Url = "Java";
             categoryInert.Description = "Categoria de serviços da Java";
-            categoryInert.Order = 12;
+            categoryInert.Order = 13;
             categoryInert.Summary = "Java";
             categoryInert.Featured = false;
 
             IConexao conexao = new Conexao();
-            SqlCommand command;
-            SqlDataReader reader;
 
-            var insertSql = @$"INSERT INTO 
+            using (var conn = conexao.AbrirConexao())
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = conn;
+
+                    try
+                    {
+
+                        // insert 
+                        var insertSql = @$"INSERT INTO 
                     [Category]
                         VALUES
-                        (   
-                            @Id,
-                            @Title,
-                            @Url,
-                            @Summary,
-                            @Order,
-                            @Description,
-                            @Featured
+                        (  
+                            @Id,  @Title, @Url,  @Summary, @Order, @Description, @Featured
+
                         )";
 
-            
-            try
-            {   
-                command = new SqlCommand(insertSql, conexao.AbrirConexao());
-                command.Parameters.AddWithValue("@Id", categoryInert.Id);
-                command.Parameters.AddWithValue("@Title", categoryInert.Title);
-                command.Parameters.AddWithValue("@Url", categoryInert.Url);
-                command.Parameters.AddWithValue("@Summary", categoryInert.Summary);
-                command.Parameters.AddWithValue("@Order", categoryInert.Order);
-                command.Parameters.AddWithValue("@Description", categoryInert.Description);
-                command.Parameters.AddWithValue("@Featured", categoryInert.Featured);
-                var linhaAfetadas = command.ExecuteNonQuery();
-                Console.WriteLine($"Linhas afetadas -  {linhaAfetadas}");
-            }
-            catch (Exception ex)
-            {
+                        command.Parameters.AddWithValue("@Id", categoryInert.Id);
+                        command.Parameters.AddWithValue("@Title", categoryInert.Title);
+                        command.Parameters.AddWithValue("@Url", categoryInert.Url);
+                        command.Parameters.AddWithValue("@Summary", categoryInert.Summary);
+                        command.Parameters.AddWithValue("@Order", categoryInert.Order);
+                        command.Parameters.AddWithValue("@Description", categoryInert.Description);
+                        command.Parameters.AddWithValue("@Featured", categoryInert.Featured);
 
-                Console.WriteLine($"E501 - Erro ao cadastrar categoria");
-                Console.WriteLine($"Mensagem: {ex.Message}");
-            }
+                        command.CommandText = insertSql;
+                        var linhasAfetadas = command.ExecuteNonQuery();
+                        Console.WriteLine($"{linhasAfetadas} registr afetas");
 
-            var selectSql = "SELECT [Id], [Title] FROM [Category] ORDER BY 2";
-            
+                        // select
+                        var selectSql = "SELECT [Id], [Title] FROM [Category] WHERE Id = @Id ORDER BY 2";
+                        command.CommandText = selectSql;
+                        var reader = command.ExecuteReader();
 
-            try
-            {   
-                command = new SqlCommand(selectSql, conexao.AbrirConexao());
-                reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    var categorySelect = new Category();
+                        while (reader.Read())
+                        {
+                            var category = new Category();
 
-                    categorySelect.Id = reader.GetGuid(0);
-                    categorySelect.Title = reader.GetString(1);
+                            category.Id = reader.GetGuid(0);
+                            category.Title = reader.GetString(1);
+                            Console.WriteLine($"id: {category.Id}, title: {category.Title}");
+                        }
+                    }
+                    catch (System.Exception ex)
+                    {
 
-                    Console.WriteLine($"id: {categorySelect.Id}, title: {categorySelect.Title}");
+                        Console.WriteLine($"Erro ao cadastrar categoria");
+                        Console.WriteLine($"Mensagem: {ex.Message}");
+                    }
+                    conexao.FecharConexao();
                 }
-                reader.Close();
+
             }
-            catch (Exception ex)
-            {
-
-                Console.WriteLine($"E502 - Erro ao buscar categoria");
-                Console.WriteLine($"Mensagem: {ex.Message}");
-            }
-
-
-
-            conexao.FecharConexao();
 
         }
     }
+
 }
+
