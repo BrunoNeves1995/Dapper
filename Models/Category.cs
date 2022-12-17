746a58bad63205abdea459612110e5a2d241c2f2
Bruno Neves <nevesbruno814@gmail.com>
2022-12-17 09:59:12 -0400

    funcionando porem tenho que abrir duas conexões com o banco de dados para executar

--- Models/Category.cs
+++ Models/Category.cs
@@ -7,13 +7,14 @@ namespace Dapper.Models
 {
     public class Category
     {
-        public Category(Guid id, string title)
-        {
-            Id = id;
-            Title = title;
-        }
+       
 
-        public Guid Id { get; private set; }
-        public string? Title { get; private set; }
+        public Guid Id { get;  set; }
+        public string? Title { get;  set; }
+        public string? Url { get;  set; }
+        public string? Summary { get;  set; }
+        public int Order { get;  set; }
+        public string? Description { get;  set; }
+        public bool Featured { get;  set; }
     }
 }
